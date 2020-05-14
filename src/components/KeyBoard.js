545a3f5cc0c3e83585008c5436fc75a0d730a515
Nathan Engler <englernathan@gmail.com>
2020-05-14 15:05:25 -0500

    first commit

---
+++ src/components/KeyBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class KeyBoard extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>inside keyboard</h2>
+      </div>
+    );
+  }
+}
+
+export default KeyBoard;
---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Results extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>Inside results</h2>
+      </div>
+    );
+  }
+}
+
+export default Results;
