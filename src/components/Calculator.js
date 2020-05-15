a5bdb82044a3e9a07c4ca803cec16e1d949317ab
Nathan Engler <englernathan@gmail.com>
2020-05-15 16:05:55 -0500

    added = functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,33 +9,87 @@ class Calculator extends Component {
     errorMessage: "",
   };
 
+  calculateTotal() {
+    const words = this.state.userInput.split(" ");
+    let answer = parseInt(words[0]);
+    words.forEach((word, i) => {
+      if (i % 2 === 1) {
+        switch (word) {
+          case "+":
+            answer += parseInt(words[i + 1]);
+            break;
+          case "-":
+            answer -= parseInt(words[i + 1]);
+            break;
+          case "x":
+            answer *= parseInt(words[i + 1]);
+            break;
+          case "÷":
+            answer /= parseInt(words[i + 1]);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+    this.setState({ answer });
+  }
+
   handleUserInput = (value) => {
     this.setState({ errorMessage: "" });
     const signs = ["+", "-", "x", "÷"];
     let { userInput } = this.state;
-    if (value !== "C" && value !== "←" && value !== "=") {
-      if (
-        signs.includes(
-          userInput.substring(userInput.length - 1, userInput.length)
-        ) &&
-        signs.includes(value)
-      ) {
-        this.setState({
-          userInput: userInput.slice(0, userInput.length - 1) + value,
-        });
-      } else if (signs.includes(value) && userInput.length === 0) {
-        this.setState({ errorMessage: "Cannot start with a sign" });
-      } else {
+    switch (value) {
+      case "C":
+        this.setState({ answer: 0, userInput: "" });
+        break;
+      case "←":
+        if (
+          signs.includes(
+            userInput.substring(userInput.length - 2, userInput.length - 1)
+          )
+        ) {
+          this.setState({
+            userInput: userInput.slice(0, userInput.length - 3),
+          });
+        } else {
+          this.setState({
+            userInput: userInput.slice(0, userInput.length - 1),
+          });
+        }
+        break;
+      case "=":
+        if (
+          signs.includes(
+            userInput.substring(userInput.length - 2, userInput.length - 1)
+          )
+        ) {
+          this.setState({ errorMessage: "Cannot end with a sign" });
+        } else {
+          this.calculateTotal();
+        }
+        break;
+      case "+":
+      case "-":
+      case "x":
+      case "÷":
+        if (
+          signs.includes(
+            userInput.substring(userInput.length - 2, userInput.length - 1)
+          ) &&
+          signs.includes(value)
+        ) {
+          this.setState({
+            userInput: userInput.slice(0, userInput.length - 2) + value + " ",
+          });
+        } else if (signs.includes(value) && userInput.length === 0) {
+          this.setState({ errorMessage: "Cannot start with a sign" });
+        } else {
+          this.setState({ userInput: userInput + " " + value + " " });
+        }
+        break;
+      default:
         this.setState({ userInput: userInput + value });
-      }
-    } else if (value === "C") {
-      this.setState({ answer: 0, userInput: "" });
-    } else if (value === "←") {
-      this.setState({
-        userInput: userInput.slice(0, userInput.length - 1),
-      });
-    } else if (value === "=") {
-      console.log(userInput);
     }
   };
 
